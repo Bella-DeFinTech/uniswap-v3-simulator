94e5ebc166454d12a977f516e67bc6c45b183643
kafeikui <kkffkk3@protonmail.com>
2021-08-20 22:18:59 +0800

    correct method signature as core pool contract

--- src/core/CorePool.ts
+++ src/core/CorePool.ts
@@ -1,4 +1,4 @@
-import JSBI from 'jsbi';
+import JSBI from "jsbi";
 import { TickManager } from "../manager/TickManager";
 import { PositionManager } from "../manager/PositionManager";
 import { Position } from "../model/Position";
@@ -77,15 +77,16 @@ export class CorePool {
     this._sqrtPriceX96 = sqrtPriceX96;
   }
 
-  mint(tickLower: number, tickUpper: number, amount: JSBI) {
+  mint(recipient: string, tickLower: number, tickUpper: number, amount: JSBI) {
     // TODO
   }
 
-  burn(tickLower: number, tickUpper: number, amount: JSBI) {
+  burn(owner: string, tickLower: number, tickUpper: number, amount: JSBI) {
     // TODO
   }
 
   collect(
+    recipient: string,
     tickLower: number,
     tickUpper: number,
     amount0Requested: JSBI,
@@ -99,20 +100,27 @@ export class CorePool {
   }
 
   private modifyPosition(
+    owner: string,
     tickLower: number,
     tickUpper: number,
     liquidityDelta: JSBI
-  ): { amount0: JSBI; amount1: JSBI } {
+  ): { position: Position; amount0: JSBI; amount1: JSBI } {
     // TODO
-    return { amount0: JSBI.BigInt(0), amount1: JSBI.BigInt(0) };
+    return {
+      position: new Position(),
+      amount0: JSBI.BigInt(0),
+      amount1: JSBI.BigInt(0),
+    };
   }
 
   private updatePosition(
+    owner: string,
     tickLower: number,
     tickUpper: number,
     liquidityDelta: JSBI
-  ) {
+  ): Position {
     // TODO
+    return new Position();
   }
 
   getTick(tick: number): Tick {
